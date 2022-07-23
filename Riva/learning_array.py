4eabbcabc22b560a1b0523059225ce530aaa7dbc
rivapatel123 <riva.patel1316@gmail.com>
2022-07-23 15:53:42 -0400

    learning array :D

---
+++ Riva/learning_array.py
@@ -0,0 +1,35 @@
+jan = "january"
+print(jan)
+
+feb = "febuary"
+print(feb)
+
+mar = "march"
+print(mar)
+
+apr = "april"
+print(apr)
+
+may = "may"
+print(may)
+
+june = "june"
+print(june)
+
+july = "july"
+print(july)
+
+aug = "august"
+print(aug)
+
+sep = "september"
+print(jan)
+
+oct = "october"
+print(oct)
+
+nov = "november"
+print(nov)
+
+dec = "december"
+print(dec)
---
+++ Riva/learning_array_2.py
@@ -0,0 +1,16 @@
+
+months_of_the_year = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print(months_of_the_year[3])
+
+len_moy = str(len(months_of_the_year))
+
+print("There are " + len_moy + " months of the year.")
+
+month_one = str(months_of_the_year[0])
+print("The first month of the year is " + month_one + ".")
+
+month_last = str(months_of_the_year[11])
+print("The last month of the year is " + month_last + ".")
+
+
