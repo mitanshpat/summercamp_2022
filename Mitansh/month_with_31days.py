9b6697bffdcaa9db9d960f20e64badb1198f27fd
Mitansh <mitansh.patel26@gmail.com>
2022-07-23 16:27:54 -0400

    Month with 31 days

---
+++ Mitansh/month_with_31days.py
@@ -0,0 +1,6 @@
+month_of_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print('Months with 31 days: ')
+for x in range (0,12):
+    if x == 0 or x == 2 or x == 4 or x == 6 or x == 7 or x == 9 or x == 11:
+        print(month_of_year[x])
+# 0, 2, 4, 6, 7, 9, 11
