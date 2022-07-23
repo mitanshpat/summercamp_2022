40e06854360da253b350e5b504c779db63f31e6b
Mitansh <mitansh.patel26@gmail.com>
2022-07-23 18:23:54 -0400

    Even months of the year

---
+++ Mitansh/even_months.py
@@ -0,0 +1,5 @@
+month_of_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print('Even months: ')
+for x in range (0,12):
+    if x%2 == 0:
+        print(month_of_year[x])
