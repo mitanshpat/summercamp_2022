901b46c631ca19ca95edcc96a2c468c959dad6b8
Vaidehi <vaidehi.bhagat07@gmail.com>
2022-07-24 11:47:32 -0400

    Vaidehi push

--- Mitansh/if_ui_is_str_or_int.py
+++ Mitansh/if_ui_is_str_or_int.py
@@ -1,10 +1,19 @@
+#variable for user input: string
 user_input = input('Enter anything: ')
+#sample user_input: " 123"
+#u_i_strip will be "123"
+u_i_strip = user_input.strip()
 
-while user_input != 'quit':
-    if user_input.isnumeric():
-        print('It is a integer')
+#sample user_input: " 123"
+#u_i_strip is "123"
+while u_i_strip != 'quit':
+    #it will go inside the if
+    if u_i_strip.isnumeric():
+        #it will print "it is numeric"
+        print("it is numeric.")
+        #user_input is " 345"
         user_input = input('Enter anything: ')
     else:
-        print('It is a string')
+        print(user_input)
         user_input = input('Enter anything: ')
 print('Thanks for chatting with me')
