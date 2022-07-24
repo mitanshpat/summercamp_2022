7f91ea2fbfba70021ab87937d909c19cecc64cc0
Vaidehi <vaidehi.bhagat07@gmail.com>
2022-07-24 11:26:26 -0400

    Vaidehi push

--- Vaidehi/python_countinous_string_or_integer.py
+++ Vaidehi/python_countinous_string_or_integer.py
@@ -1,8 +1,8 @@
 user_input = input("Enter something: ")
-numeric = user_input.isnumeric()
+u_i_strip = user_input.strip()
 
-while user_input != "quit":
-    if numeric == True:
+while user_input.lower() != "quit":
+    if u_i_strip.isnumeric() == True:
         print(user_input + " is a number.")
     else:
         print(user_input + " is a string.")
