7fe239b9cb59bec650ca5319c8971e77e17cfc14
Mitansh <mitansh.patel26@gmail.com>
2022-07-26 22:58:52 -0400

    Arranging numbers in order from array.

---
+++ Mitansh/array_numb_in_order.py
@@ -0,0 +1,17 @@
+user_i = int(input('Enter the first number: '))
+user_i_2 = int(input('Enter the second number: '))
+user_i_3 = int(input('Enter the third number: '))
+user_i_4 = int(input('Enter the fourth number: '))
+user_i_5 = int(input('Enter the fifth number: '))
+user_i_6 = int(input('Enter the sixth number: '))
+user_i_7 = int(input('Enter the seventh number: '))
+user_i_8 = int(input('Enter the eighth number: '))
+user_i_9 = int(input('Enter the ninth number: '))
+user_i_last = int(input('Enter the last number: '))
+
+
+numb_ord = [user_i, user_i_2, user_i_3, user_i_4, user_i_5, user_i_6, user_i_7, user_i_8, user_i_9, user_i_last]
+
+num = sorted(numb_ord)
+print(num)
+
