f6325671bbf832bd6627e7ca9e22f4803b974cfb
Mitansh <mitansh.patel26@gmail.com>
2022-07-29 11:16:48 -0400

    Mitansh pushed this file

---
+++ Mitansh/def_function.py
@@ -0,0 +1,15 @@
+user_input_amount = input('Enter the number of times you want your name printed: ')
+user_input_name = input('Enter your name: ')
+
+def user_i (user_input_amount, user_input_name):
+    for user_i in range(int(user_input_amount)):
+        print(user_input_name)
+
+user_i(user_input_amount, user_input_name)
+
+
+def user_i (user_input_amount, user_input_name):
+    for user_i in range(int(user_input_amount)):
+        print(user_input_name)
+
+user_i(user_input_amount, user_input_name)
